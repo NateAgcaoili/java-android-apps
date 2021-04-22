2fff9d8078744509790c9d858124d333abcd0bf6
NateAgcaoili <75859423+NateAgcaoili@users.noreply.github.com>
2021-04-22 18:03:19 -0400

    Lists available android sensors

--- SensorDemo/app/src/main/java/com/georgiasouthern/sensordemo/MainActivity.java
+++ SensorDemo/app/src/main/java/com/georgiasouthern/sensordemo/MainActivity.java
@@ -2,13 +2,26 @@ package com.georgiasouthern.sensordemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    SensorManager sm;
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.textView);
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor s : sensors) {
+            String type = s.getStringType();
+            tv.append(type + "\n");
+        }
     }
 }
